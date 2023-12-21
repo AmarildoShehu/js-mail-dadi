516eca5a9e508b909e424d5d0705db783d7f4c30
Amarildo <amarildos@outloo.it>
2023-12-21 18:25:32 +0100

    created function, added style

--- js/script.js
+++ js/script.js
@@ -7,4 +7,26 @@ const computerElm = document.querySelector('.computer');
 const playbtnElm = document.querySelector('.playbtn');
 const winnerElm = document.querySelector('.winner');
 
+// 5. creo la funzione del bottone
+playbtnElm.addEventListener('click', gioca);
 
+function gioca() {
+    let playerTurn = Math.floor(Math.random() * 6) + 1;
+    console.log(playerTurn);
+    let computerTurn = Math.floor(Math.random() * 6) + 1;
+    console.log(computerTurn);
+  
+    playerElm.textContent = playerTurn;
+    computerElm.textContent = computerTurn;
+  
+    let winnerText;
+    if (playerTurn === computerTurn) {
+        winnerText = "Pareggio!";
+    } else if (playerTurn > computerTurn) {
+        winnerText = "Player!";
+    } else {
+        winnerText = "Computer!";
+    }
+
+    winnerElm.textContent = "Il vincitore è: " + winnerText;
+  }
