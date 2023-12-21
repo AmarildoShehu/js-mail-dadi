dc4eff0e725c5e7784bd7759e03be560dcd173eb
Amarildo <amarildos@outloo.it>
2023-12-21 16:41:33 +0100

    taken classes in js from index

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 //1. agganciamo tutti i file tra cui html e js;
 console.log('JS OK');
 // -----------------------------//
+// 4. recupero le classi in js
+const playerElm = document.querySelector('.player');
+const computerElm = document.querySelector('.computer');
+const playbtnElm = document.querySelector('.playbtn');
+const winnerElm = document.querySelector('.winner');
+
 
